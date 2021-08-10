8dd59224b56e3bc2ce6287f66bdd22ab4c4a6787
Daniel Soares <daniel.soares@me.com>
2021-08-10 18:11:51 -0300

    fix mongoose module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 
 import { TeamsModule } from './teams/teams.module'
 import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [UsersModule, TeamsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27018/deimos', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }),
+    UsersModule,
+    TeamsModule
+  ],
   controllers: [],
   providers: []
 })
