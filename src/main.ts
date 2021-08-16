19f13aad55a3542a2f1c8c5bc92c3009b9f9757d
Daniel Soares <daniel.soares@me.com>
2021-08-16 01:20:08 -0300

    fix Stripe parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core'
+import express, { NextFunction, Request, Response } from 'express'
 
 import { AppModule } from './app.module'
 import { AllExceptionsFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true, bodyParser: true })
+  const app = await NestFactory.create(AppModule, { cors: true, bodyParser: false })
+
+  app.use((req: Request, res: Response, next: NextFunction): void => {
+    if (req.originalUrl === '/subscriptions/webhooks') {
+      express.raw({ type: 'application/json' })
+      next()
+    } else {
+      express.json()(req, res, next)
+    }
+  })
+
   app.useGlobalFilters(new AllExceptionsFilter())
+
   await app.listen(Number(process.env.PORT) ?? 3010)
 }
 
