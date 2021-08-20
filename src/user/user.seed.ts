75c4347757ca2ff57a7100cbb8f0ac0aa02bb659
Daniel Soares <daniel.soares@me.com>
2021-08-20 14:27:46 -0300

    fix ci process

--- src/user/user.seed.ts
+++ src/user/user.seed.ts
@@ -9,5 +9,23 @@ export default class CreateUsers implements Seeder {
     await factory(User)().createMany(10, { role: Role.account_manager })
     await factory(User)().createMany(5, { role: Role.supervisor })
     await factory(User)().createMany(5, { role: Role.admin })
+
+    await factory(User)().create({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: process.env.USER_ADMIN_EMAIL,
+      password: process.env.USER_ADMIN_PASS,
+      role: Role.admin,
+      verified: true
+    })
+
+    await factory(User)().create({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: process.env.USER_CLIENT_EMAIL,
+      password: process.env.USER_CLIENT_PASS,
+      role: Role.client,
+      verified: true
+    })
   }
 }
